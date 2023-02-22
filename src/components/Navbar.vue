673f724ae137245b102ca06a1caa2a09f80d46ab
houcem <hedhoucem@gmail.com>
2023-02-22 12:39:40 +0100

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="nav-bar"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.nav-bar {
+  background: linear-gradient(-90deg, #b92b27, #1565C0);
+  height: 60px;
+  margin-bottom: 25px;
+  -webkit-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
+  -moz-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.57);
+}
+</style>
